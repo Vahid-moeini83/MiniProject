35a341161e5779669b89f5fe6c3a7f4207a8c2c6
Vahid-moeini83 <vahid.asdfghjkl.moeini@gmail.com>
2023-09-21 12:34:41 +0330

    going for last section

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+// // Swiper JS slider
+// var swiper = new swiper(".top-slider", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
